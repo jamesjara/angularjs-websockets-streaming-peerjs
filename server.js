3baf895b40a548dc5a98d32139b092749fd4746c
jamesjara <jamesjara@gmail.com>
2017-02-09 02:05:50 +0000

    forget to add dynamically port

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
-
 app.use('/app', express.static(__dirname + '/src'));
-
-
-
 app.get('/', function(req,res){
   res.sendFile(path.join(__dirname, 'src/index.html'));
 });
 
-app.listen(3003);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
